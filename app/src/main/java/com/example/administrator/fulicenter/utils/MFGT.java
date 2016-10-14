9c93c32636d0396254bdaaf6e1be9b3baf14c10d
lixiaopang22 <1021733560@qq.com>
2016-10-14 16:03:03 +0800

    添加MFGT工具类，并在SplashActivity中测试

---
+++ app/src/main/java/com/example/administrator/fulicenter/utils/MFGT.java
@@ -0,0 +1,24 @@
+package com.example.administrator.fulicenter.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.example.administrator.fulicenter.R;
+import com.example.administrator.fulicenter.activity.MainActivity;
+
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void gotoMainActivity(Activity context){
+        startActivity(context, MainActivity.class);
+    }
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+}
