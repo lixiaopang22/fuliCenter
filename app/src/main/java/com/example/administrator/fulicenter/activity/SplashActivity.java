4f69f72cc56bc84e509a984f730cba055190c8ac
lixiaopang22 <1021733560@qq.com>
2016-10-14 20:12:56 +0800

    修改闪屏界面进入主界面的过程中出现桌面及没有动画的bug

--- app/src/main/java/com/example/administrator/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/com/example/administrator/fulicenter/activity/SplashActivity.java
@@ -1,13 +1,14 @@
 package com.example.administrator.fulicenter.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.administrator.fulicenter.R;
+import com.example.administrator.fulicenter.utils.MFGT;
 
 public class SplashActivity extends AppCompatActivity {
-    static final long sleepTime=2000;
+    static final long sleepTime=5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,22 +18,32 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        new Thread(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                long start = System.currentTimeMillis();
-                long costTime = System.currentTimeMillis() - start;
-                if(sleepTime-costTime>0){
-                    try {
-                        //耗时操作
-                        Thread.sleep(sleepTime-costTime);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                MFGT.gotoMainActivity(SplashActivity.this);
+                //MFGT.finish(SplashActivity.this);
                 finish();
             }
-        }).start();
+        },sleepTime);
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                long start = System.currentTimeMillis();
+//                long costTime = System.currentTimeMillis() - start;
+//                if(sleepTime-costTime>0){
+//                    try {
+//                        //耗时操作
+//                        Thread.sleep(sleepTime-costTime);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+//                finish();
+//                MFGT.gotoMainActivity(SplashActivity.this);
+//                MFGT.finish(SplashActivity.this);
+//            }
+//        }).start();
     }
 }
