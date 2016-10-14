ac4aba7d6f0d1d67436cd651ec89b03a3ac9e33c
lixiaopang22 <1021733560@qq.com>
2016-10-14 21:46:43 +0800

    在MainActivity中添加onCheckedChange方法

--- app/src/main/java/com/example/administrator/fulicenter/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/fulicenter/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.fulicenter.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.administrator.fulicenter.R;
 import com.example.administrator.fulicenter.utils.L;
@@ -13,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         L.i("main","MainActivity onCreate()");
+    }
+    public void onCheckedChange(View view){
+
     }
 }
