4c97272f6fd6409a231e809853a5cb99168dbb7f
lixiaopang22 <1021733560@qq.com>
2016-10-17 11:28:04 +0800

    删除buttongroup，在MainActivity里实现按键互斥

--- app/src/main/java/com/example/administrator/fulicenter/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/fulicenter/activity/MainActivity.java
@@ -3,19 +3,75 @@ package com.example.administrator.fulicenter.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
 
 import com.example.administrator.fulicenter.R;
 import com.example.administrator.fulicenter.utils.L;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.newGoods)
+    RadioButton newGoods;
+    @BindView(R.id.Boutique)
+    RadioButton Boutique;
+    @BindView(R.id.Category)
+    RadioButton Category;
+    @BindView(R.id.Cart)
+    RadioButton Cart;
+    @BindView(R.id.Center)
+    RadioButton Center;
+
+    int index;
+    RadioButton [] rbs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        L.i("main","MainActivity onCreate()");
+        ButterKnife.bind(this);
+        L.i("main", "MainActivity onCreate()");
+        initView();
     }
-    public void onCheckedChange(View view){
 
+    private void initView() {
+        rbs=new RadioButton[5];
+        rbs [0]=newGoods;
+        rbs [1]=Boutique;
+        rbs [2]=Category;
+        rbs [3]=Cart;
+        rbs [4]=Center;
+    }
+
+    public void onCheckedChange(View view) {
+        switch(view.getId()){
+            case R.id.newGoods :
+                index=0;
+                break;
+            case R.id.Boutique :
+                index=1;
+                break;
+            case R.id.Category :
+                index=2;
+                break;
+            case R.id.Cart :
+                index=3;
+                break;
+            case R.id.Center :
+                index=4;
+                break;
+        }
+        setRadioButtonStatus();
+    }
+
+    private void setRadioButtonStatus() {
+        for(int i=0;i<rbs.length;i++){
+            if(i==index){
+                rbs[i].setChecked(true);
+            }else{
+                rbs[i].setChecked(false);
+            }
+        }
     }
 }
